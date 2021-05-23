2b6cf24530083655b19ec0d232867154263fdfec
kruthik23 <gskruthik@gmail.com>
2021-05-24 03:07:34 +0530

    adding

---
+++ SwitchDemo.java
@@ -0,0 +1,24 @@
+package com.onebill;
+
+public class SwitchDemo {
+
+public static void main(String args[]) {
+	for(int i=0;i<=7 ;i++) {
+		switch(i) {
+		
+		case 0 : System.out.println(i);
+		 break;
+		case 1 : System.out.println(i);
+		break;
+		case 2 : System.out.println(i);
+		break;
+		case 3 : System.out.println(i);
+		break;
+		case 4 : System.out.println(i);
+		break;
+			default :System.out.println("this is default");
+		}
+		System.out.println();
+	}
+}
+}
