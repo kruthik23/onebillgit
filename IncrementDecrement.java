62eebdd21d9e9a43561e1390a622020ee7938d60
kruthik23 <gskruthik@gmail.com>
2021-05-24 03:04:35 +0530

    adding

---
+++ IncrementDecrement.java
@@ -0,0 +1,22 @@
+package com.onebill;
+
+public class IncrementDecrement {
+public static void main(String args[]) {
+	for(int i=0 ; i<7 ;  i++) {
+		switch(i) {
+		case 0 : System.out.println("i less than "+ i);
+		         break;
+		case 1 : System.out.println("i is less than "+ i);
+		         break;
+		case 2 : System.out.println("i is less than "+i);
+		         break;
+		case 3 : System.out.println("i is less than "+i);
+		         break;
+		case 4 : System.out.println("i is less than "+i);
+		         break;
+		default :System.out.println("this is default");         
+		}
+		System.out.println(" ");
+	}
+}
+}
