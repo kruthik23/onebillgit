36dd807e9599e030ed4dd7a7f26a320135a7ad6d
kruthik23 <84615226+kruthik23@users.noreply.github.com>
2021-06-01 19:10:26 +0530

    Add files via upload
    adding

---
+++ Assessment/GreaterNum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class GreaterNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a");
+   a=sc.nextInt();
+   System.out.println("enter b");
+   b=sc.nextInt();
+   System.out.println("enter c");
+   c=sc.nextInt();
+   if(a>b) {
+	   if(a>c)
+		   System.out.println("a is greater");
+	   else 
+		   System.out.println("c is greater");
+   }else {
+	   if(b>c)
+   System.out.println("b is greater");
+	   else
+		   System.out.println("c is greater");
+	}
+
+}
+}
