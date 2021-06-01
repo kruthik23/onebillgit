0b3a983ae6ba413de1351ea73973ee0d3ae89f31
kruthik23 <84615226+kruthik23@users.noreply.github.com>
2021-06-01 20:10:39 +0530

    Update OBDBBankTable.java

--- OBDBBankTable.java
+++ OBDBBankTable.java
@@ -13,7 +13,7 @@ public class OBDBBankTable {
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/OneBillbankdb?user=onebill & password= onebill");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/OneBillbankdb?user=root & password= Onebill@2020");
 			String query = "create table employee(empid int primary key, empname varchar(50), empsalary int)";
 			stmt = con.createStatement();
 			int count = stmt.executeUpdate(query);
